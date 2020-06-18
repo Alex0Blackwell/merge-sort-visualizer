7593310cf3fde4428ccb815135b272db0305d62e
alex0blackwell <alex.blackwell314@gmail.com>
2020-06-18 12:49:32 -0700

    Animation working, style update, readme and license update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Merge-sort-graph
+# Merge Sort Visualizer
+
+## Table of contents
+* [Overview](#overview)
+* [Examples](#examples)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Limitations](#limitations)
+
+## Overview
+This project is a website that visualizes the steps of a merge sort. The algorithm sorts columns of differing heights in ascending order. The initial order of the columns is random and can be randomized when the merge sort is not running. This program can help to understand the steps the merge sort algorithm takes.
+
+## Examples
+
+> Recording of the sorting animation on desktop
+
+![Screenshot](pictures/mergeVid.gif "My mouse makes a cameo")
+
+> Screenshot on mobile during the sort animation
+
+![Screenshot](pictures/mobile.png "Super Speed!")
+
+## Technologies
+- **HTML / CSS**
+- **JavaScript**
+- **Bootstrap 4.5**
+
+## Usage
+Find the game [here](https://alex0blackwell.github.io/pages/merge/) or at the *portfolio* section of my [website](https://alex0blackwell.github.io/).
+
+## Limitations
+- [ ] No sorting algorithms other than merge sort
+
+## License
+Licensed under the [GNU General Public License v3.0](LICENSE)
--- js/main.js
+++ js/main.js
@@ -1,103 +1,85 @@
 
 function merge(arr, start, end, split) {
-    var _tmpArr = [];
-    var left, right, count;
-    count = 0;
-    left = start;
-    right = split + 1;
+  return new Promise(resolve => {
+    setTimeout(() => {
+      var _tmpArr = [];
+      var left, right, count;
+      count = 0;
+      left = start;
+      right = split + 1;
 
-    while(left <= split && right <= end) {
-      if(arr[left] <= arr[right]) {
-        _tmpArr[count++] = arr[left++];
-      }
-      else if(arr[right] < arr[left]) {
-        _tmpArr[count++] = arr[right++];
-      }
-    }
-    // now flush anything left in the left and right of the array
-    while(left <= split) {
-      _tmpArr[count++] = arr[left++];
-    }
-    while(right <= end) {
-      _tmpArr[count++] = arr[right++];
-    }
-    // copy the temp array over
-    count = 0;
-    var svg = document.getElementById("SVG");
-    var toRemove = document.querySelectorAll('rect[x]');
-
-     for(let i = start; i <= end; i++) {
-        arr[i] = _tmpArr[count++];
-       task(i);
-     }
-
-     function task(i) {
-       setTimeout(function() {
-         console.log("count = "+count);
-         console.log("arr = "+arr);
-         console.log("_tmpArr = "+_tmpArr);
-         for(var r = 0; r < toRemove.length; r++) {
-           //console.log(`attribute = ${toRemove[r].getAttribute("x")}, and x = ${((i+1)*15).toString()}`);
-           if(toRemove[r].getAttribute("x") == ((i+1)*15).toString()) {
-             break;
-           }
-         }
-         //console.log("so, r = "+r);
-         // now find the one with the correct x value to remove it
-         svg.removeChild(toRemove[r]);
-         //svg.removeChild(svg.childNodes[i]);
-         shape = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    
-         shape.setAttribute("x", (i+1)*15);
-         shape.setAttribute("y", 250-arr[i]);
-         shape.setAttribute("id", "rect"+i);
-         shape.setAttribute("width", 15);
-         shape.setAttribute("height", arr[i]);
-         shape.setAttribute("fill", "grey");
-         svg.appendChild(shape);
-       }, 250 * i);
-     }
-/*
-    for(var i = start; i <= end; i++) {
-      arr[i] = _tmpArr[count++];
-
-      for(var r = 0; r < toRemove.length; r++) {
-        if(toRemove[r].getAttribute("x") == ((i+1)*15).toString()) {
-          break;
+      while(left <= split && right <= end) {
+        if(arr[left] <= arr[right]) {
+          _tmpArr[count++] = arr[left++];
+        }
+        else if(arr[right] < arr[left]) {
+          _tmpArr[count++] = arr[right++];
         }
       }
-      // now find the one with the correct x value to remove it
-      svg.removeChild(toRemove[r]);
-      //svg.removeChild(svg.childNodes[i]);
-      shape = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+      // now flush anything left in the left and right of the array
+      while(left <= split) {
+        _tmpArr[count++] = arr[left++];
+      }
+      while(right <= end) {
+        _tmpArr[count++] = arr[right++];
+      }
+      // copy the temp array over
+      count = 0;
+      var svg = document.getElementById("SVG");
+      var toRemove = svg.children;
 
-      shape.setAttribute("x", (i+1)*15);
-      shape.setAttribute("y", 250-arr[i]);
-      shape.setAttribute("id", "rect"+i);
-      shape.setAttribute("width", 15);
-      shape.setAttribute("height", arr[i]);
-      shape.setAttribute("fill", "grey");
-      svg.appendChild(shape);
+      for(let i = start; i <= end; i++) {
+        arr[i] = _tmpArr[count++];
+        task(i);
+      }
 
-    }
-*/
-    return;
+      function task(i) {
+        setTimeout(function() {
+          for(var r = 0; r < toRemove.length; r++) {
+            if(toRemove[r].getAttribute("x") == ((i+1)*15).toString()) {
+              break;
+            }
+          }
+          // now find the one with the correct x value try to remove it
+          try {
+            svg.removeChild(document.getElementById(toRemove[r].id));
+          } catch (e) {
+            console.log("could not find child of svg with id:", toRemove[r].id);
+          }
+          shape = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+          shape.setAttribute("x", (i+1)*15);
+          shape.setAttribute("y", 250-arr[i]);
+          shape.setAttribute("id", "rect"+i);
+          shape.setAttribute("width", 15);
+          shape.setAttribute("rx", 2)
+          shape.setAttribute("ry", 2)
+          shape.setAttribute("height", arr[i]);
+          svg.appendChild(shape);
+        }, 10*i);
+      }
+      resolve("Resolved");
+    },200);
+  });
 }
 
 
-function mergeSort(arr, start, end) {
+async function mergeSort(arr, start, end) {
   if(start >= end) {
-    return;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve("resolved");
+      },100);
+    });
   }
   var split = Math.floor((start + end) / 2);
+  let res;
 
-  mergeSort(arr, start, split);  // sort left side
-  // setTimeout(mergeSort, 250, arr, start, split);
-    mergeSort(arr, split+1, end);  // sort right side
-   //setTimeout(mergeSort, 250, arr, split+1, end);
-   merge(arr, start, end, split);  // merge them together
-  // setTimeout(merge, 1000, arr, start, end, split);
+  res = await mergeSort(arr, start, split);  // sort left side
 
+  res = await mergeSort(arr, split+1, end);  // sort right side
+
+  res = await merge(arr, start, end, split);  // merge them together
 }
 
 
@@ -121,10 +103,11 @@ function randomize() {
 
 var randomArr = [];
 var barHeights = [];
-function main(sort) {
+async function main(sort) {
   if(!sort) {
+    randomArr = [];
     randomArr = randomize();
-    barArr = [];
+    barHeights = barArr = [];
     var shape, svg, c;
     svg = document.getElementById("SVG");
     c = 0;
@@ -141,6 +124,8 @@ function main(sort) {
       shape.setAttribute("y", 250-randomArr[c]*10);
       shape.setAttribute("id", "rect"+i);
       shape.setAttribute("width", 15);
+      shape.setAttribute("rx", 2)
+      shape.setAttribute("ry", 2)
       shape.setAttribute("height", randomArr[c]*10);
       shape.setAttribute("fill", "grey");
 
@@ -150,9 +135,11 @@ function main(sort) {
       document.getElementById("SVG").appendChild(shape);
     }
   } else {
-    //setTimeout(mergeSort, 1000, barHeights, 0, 24);
-    mergeSort(barHeights, 0, 24);
-    console.log("end merge");
+    document.getElementById("randomBtn").disabled = true;
+    document.getElementById("mergeBtn").disabled = true;
+    const res = await mergeSort(barHeights, 0, 24);
+    document.getElementById("randomBtn").disabled = false;
+    document.getElementById("mergeBtn").disabled = false;
   }
 }
 
